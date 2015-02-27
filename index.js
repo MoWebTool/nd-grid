ad66e5fd9718fbe52487e67fe5fe6369d9eb928e
crossjs <liwenfu@crossjs.com>
2015-02-27 13:51:18 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @module: nd-grid
+ * @author: crossjs <liwenfu@crossjs.com> - 2015-02-27 13:47:55
+ */
+
+'use strict';
+
+var Grid;
+
+module.exports = Grid;
---
+++ tests/index-spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var expect = require('expect.js');
+var Grid = require('../index');
+
+/*globals describe, it*/
+
+describe('nd-grid', function() {
+
+  it('normal usage', function() {
+
+  });
+
+});
