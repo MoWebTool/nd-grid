76d8ce0ebe5f42138871eaf7ba4d8bf8a2a01adc
crossjs <liwenfu@crossjs.com>
2016-03-07 19:41:41 +0800

    add tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -3,7 +3,7 @@
 // var $ = require('nd-jquery')
 var chai = require('chai')
 var sinonChai = require('sinon-chai')
-var util = require('../index')
+var Grid = require('../index')
 
 var expect = chai.expect
 // var sinon = window.sinon
@@ -12,10 +12,11 @@ chai.use(sinonChai)
 
 /*globals describe,it*/
 
-describe('util', function() {
+describe('Grid', function() {
 
-  it('APP_CORE', function() {
-    expect(util.APP_CORE).to.equal('3.0.0')
+  it('new Grid', function() {
+    expect(Grid).to.be.a('function')
+    expect(new Grid).to.be.a('object')
   })
 
 })
